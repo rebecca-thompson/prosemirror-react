ae1f4e8e27d3a85738eeda7194e8e47c77f8ce29
Rebecca Thompson <33927854+rebecca-thompson@users.noreply.github.com>
2022-11-23 16:09:08 +0000

    base keymap plugin

--- src/lib/prosemirror/plugins.ts
+++ src/lib/prosemirror/plugins.ts
@@ -1,7 +1,9 @@
 import { undo, redo, history } from "prosemirror-history";
 import { keymap } from "prosemirror-keymap";
+import { baseKeymap } from "prosemirror-commands"
 
 export const plugins = [
     history(),
-    keymap({"Mod-z": undo, "Mod-y": redo})
+    keymap({"Mod-z": undo, "Mod-y": redo}),
+    keymap(baseKeymap)
 ]
